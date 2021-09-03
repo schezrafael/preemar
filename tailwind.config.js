4b4f96233801a6ed02156ae320bbf0a512340bbd
shezrafael <42253384+schezrafael@users.noreply.github.com>
2021-09-03 18:49:09 -0500

    config tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    purge: [
+        './public/**/*.html',
+        './public/**/*.php'
+    ],
+    darkMode: false, //or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: []
+}
